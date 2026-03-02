f27a12dc33542e7fbfabc9396b36e4eb98b5093a
SubDark1 <saudilinux7@gmail.com>
2026-03-02 10:32:48 +0300

    Delete usage-examples.sh

--- usage-examples.sh
+++
@@ -1,8 +0,0 @@
-# Simple scan
-python3 silver.py -t https://example.com
-
-# Advanced scan with all features
-python3 silver.py -t https://example.com --stealth --tor --waf-bypass --zero-day
-
-# Scan with custom options
-python3 silver.py -t https://example.com -d 10 -o ./reports -v
