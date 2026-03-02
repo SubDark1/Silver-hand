43c9cb3ba05cabc632c8061ae9dcf4d4079d67dc
SubDark1 <saudilinux7@gmail.com>
2026-03-02 10:30:01 +0300

    Delete install-fonts.sh

--- install-fonts.sh
+++
@@ -1,8 +0,0 @@
-# تثبيت الخطوط للعرض الأمثل
-sudo apt install fonts-dejavu fonts-liberation fonts-freefont-ttf
-
-# على macOS
-brew install --cask font-dejavu
-
-# على Windows
-# تنزيل خط DejaVu من https://dejavu-fonts.org/
