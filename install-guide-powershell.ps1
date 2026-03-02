b01bc16611cfd3ea940f50709b5b5770612657ef
SubDark1 <saudilinux7@gmail.com>
2026-03-02 10:30:19 +0300

    Delete install-guide-powershell.ps1

--- install-guide-powershell.ps1
+++
@@ -1,8 +0,0 @@
-# Clone the repository
-git clone https://github.com/SayerLinux/silver.git
-cd silver
-
-# Run installation
-python -m venv venv
-venv\Scripts\activate
-pip install -r requirements.txt
